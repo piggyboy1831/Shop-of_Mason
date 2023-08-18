ecb072786db422af6dd9ca40e56cd10a554d1c65
piggyboy1831 <127430427+piggyboy1831@users.noreply.github.com>
2023-08-18 10:49:35 -0600

    script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-
+var stripe = Stripe('pk_test_51NgJv6JEGWdGSBGGi5MsU1ELs3cmi7lEWtVWAotSUymF3s9bn8LpEf1WFLNCTmKWBKHFkEgNS9qc4hiH8xF3fJvL00HFTnUmLw');
 document.getElementById('checkout-button').addEventListener('click', async () => {
     // Create a Checkout Session using your Stripe secret key
     const session = await fetch('/api/checkout', {
