929b5d0a7830ae43b8b6a0eec184701eb47ee71b
piggyboy1831 <127430427+piggyboy1831@users.noreply.github.com>
2023-08-18 23:01:31 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var stripe = Stripe('pk_live_51NgJv6JEGWdGSBGG6m79uQhK2T2fivcYYydMNJV3nLgfXq83nGYo3wBQ4X8oFyIYvbbQWVJ5nzXBZSHRBrJoySCa00QeYO6Tvo');
+var stripe = Stripe('pk_live_51Ngh9zFdGU3te0jr4oITamY8AcZa5vz7SaibWvGtEapSLmnH8aqgsrSr2Y7SGaORw3EH3LexOUCU1brT9qMv7Yyi00XlX8wT4M');
 document.getElementById('checkout-button').addEventListener('click', async () => {
   
     const session = await fetch('/api/checkout', {
